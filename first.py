576e0b8ec446f23e7024ef147a5159c705712f0b
Korgii <amrkhaledabdldayem@outlook.com>
2023-06-19 20:02:05 +0300

    Why am i doing this to myself

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("Hello Mankind")
+#-------------------------------------------------------------------------------------------
+#!Data Types#
+#?type() => Ident Type Of Printed Data
+print(type(12)) #! int => Integer Data
+print(type(12.2)) #! float => Floating Point Number or just a decimal value really
+print(type("Hello World")) #! str => String
+print(type([1,  2, 3, 4, 5])) #! list => List Inside Square Brackets
+print(type((1, 2,  3, 4, 5))) #! tuple => Tuple Inside Reg Brackets
+print(type({"One": 1, "Two": 2, "Three: 3,"})) #!dict => Dictionary
+print(type(2==2)) #! bool => Boolean Does It Make Any Sense ??
